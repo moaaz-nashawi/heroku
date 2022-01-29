3cc5c0b4592f6cb53431e849ba4978b5fd63a2c8
Moaaz Nashawi <m.moaaz.nashawi@gmail.com>
2022-01-29 21:12:35 +0100

    sdf3

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const http = require("http");
+const express = require('express')
+const app = express()
+const port = 80
 
-const hostname = "127.0.0.1";
-const port = 8000;
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-// Create HTTP server
-const server = http.createServer(function (req, res) {
-
-    // Set the response HTTP header with HTTP status and Content type
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    // Send the response body "Hello World"
-    res.end('Hello World\n');
-});
-
-// Prints a log once the server starts listening
-server.listen(port, hostname, function () {
-
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
